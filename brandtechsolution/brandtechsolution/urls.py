66863f5b3c58336c252ad8d8cd1a8c7d74dfdaf5
BigAddict <davidnjihia536@gmail.com>
2025-10-25 17:07:06 +0300

    Add appointments app and update routing
    Included the 'appointments' app in the settings and updated the URL configuration to route requests to the appointments module. Removed an unnecessary blank line in the index.html file.

--- brandtechsolution/brandtechsolution/urls.py
+++ brandtechsolution/brandtechsolution/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('brand.urls')),  # route to your app
+    path('appointments/', include('appointments.urls')),
 ]
